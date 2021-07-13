bc62bc516a48b187d5d1655a21b22dd0236fcb48
dailyworker <workingssu@gmail.com>
2021-07-13 23:33:40 +0900

    feat : divide Package Structure as MVC, Welcome Page - MasterController : controller 패키지 밑으로 위치 이동 - model/Welcome : 서브 컨트롤러인 웰컴 위치 일단, model로 변경 (추후   Domain으로 분할하고 컨트롤러로 다시 쪼갤예정)

--- inflearn/create-backend-framework/src/main/java/io/dailyworker/framework/controller/MasterController.java
+++ inflearn/create-backend-framework/src/main/java/io/dailyworker/framework/controller/MasterController.java
@@ -1,7 +1,8 @@
-package io.dailyworker.framework;
+package io.dailyworker.framework.controller;
 
+import io.dailyworker.framework.model.Welcome;
 import java.io.IOException;
-import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,11 +16,10 @@ public class MasterController extends HttpServlet {
   protected void service(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
 
-    String name = req.getParameter("name");
+    Welcome welcome = new Welcome();
+    String jspUrl = welcome.execute(req);
 
-    String msg = "WELCOME = " + name;
-
-    PrintWriter out = resp.getWriter();
-    out.println(msg);
+    RequestDispatcher dispatcher = req.getRequestDispatcher(jspUrl);
+    dispatcher.forward(req, resp);
   }
 }
