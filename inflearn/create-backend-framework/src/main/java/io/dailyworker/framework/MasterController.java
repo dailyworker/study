57063fb517d1a3e435a0dfba2eed13f9265eeb0a
dailyworker <workingssu@gmail.com>
2021-07-13 23:04:22 +0900

    feat : MasterController

--- inflearn/create-backend-framework/src/main/java/io/dailyworker/framework/MasterController.java
+++ inflearn/create-backend-framework/src/main/java/io/dailyworker/framework/MasterController.java
@@ -1,2 +1,25 @@
-package io.dailyworker.framework;public class MasterController {
+package io.dailyworker.framework;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MasterController extends HttpServlet {
+
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  protected void service(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+
+    String name = req.getParameter("name");
+
+    String msg = "WELCOME = " + name;
+
+    PrintWriter out = resp.getWriter();
+    out.println(msg);
+  }
 }
