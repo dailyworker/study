4d286dbd9214b714cfe78745a39f7e3197e1d20b
sinsuung <sinsuung@widerplanet.com>
2021-07-19 00:18:43 +0900

    feat : TransactionContext - controller/TransactionContext : 컨트롤러에서 DB Transaction을 처리하기 위한 Context 구현

---
+++ inflearn/create-backend-framework/src/main/java/io/dailyworker/framework/controller/TransactionContext.java
@@ -0,0 +1,59 @@
+package io.dailyworker.framework.controller;
+
+import io.dailyworker.framework.db.SQLiteJdbcTransaction;
+import io.dailyworker.framework.db.Transaction;
+
+import java.sql.SQLException;
+
+public class TransactionContext {
+
+    private static final ThreadLocal<Transaction> THREAD_LOCAL = new ThreadLocal<>();
+
+    private TransactionContext() {}
+
+    public static Transaction thread() {
+        Transaction transaction = THREAD_LOCAL.get();
+        if(hasTransaction(transaction)) {
+            return transaction;
+        }
+        transaction = createDefaultTransaction();
+        load(transaction);
+        return transaction;
+    }
+
+    static void load(Transaction transaction) {
+        THREAD_LOCAL.set(transaction);
+    }
+
+    private static Transaction createDefaultTransaction() {
+        return new SQLiteJdbcTransaction();
+    }
+
+    private static boolean hasTransaction(Transaction transaction) {
+        return transaction != null;
+    }
+
+    static void unload() {
+        Transaction transaction = THREAD_LOCAL.get();
+        if(hasTransaction(transaction)) {
+            THREAD_LOCAL.get().closeConnection();
+        }
+        THREAD_LOCAL.remove();
+    }
+
+    public static void commit() throws SQLException {
+        Transaction transaction = THREAD_LOCAL.get();
+        if(!hasTransaction(transaction)) {
+            return;
+        }
+        transaction.commit();
+    }
+
+    public static void rollback() throws SQLException {
+        Transaction transaction = THREAD_LOCAL.get();
+        if(!hasTransaction(transaction)) {
+            return;
+        }
+        transaction.rollback();
+    }
+}
