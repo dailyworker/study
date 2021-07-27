01bb859a5183d13e50680fe8260fe70280c1187c
dailyworker <seansin@chungbuk.ac.kr>
2021-07-27 21:19:19 +0900

    feat : Employeee, EmployeeRepository - 도메인 클래스인 Employee 구현 - Employee의 DAO인 EmployeeRepository 구현

--- inflearn/create-backend-framework/src/main/java/io/dailyworker/framework/domain/Employee.java
+++ inflearn/create-backend-framework/src/main/java/io/dailyworker/framework/domain/Employee.java
@@ -1,4 +1,38 @@
 package io.dailyworker.framework.domain;
 
+import io.dailyworker.framework.aop.CustomRequest;
+import io.dailyworker.framework.controller.CustomRequestContext;
+import io.dailyworker.framework.db.Table;
+
+import java.sql.SQLException;
+
 public class Employee {
+    public String select() throws SQLException {
+
+        CustomRequest customRequest = CustomRequestContext.get();
+
+        EmployeeRepository employeeRepository = new EmployeeRepository();
+        Table maybeTable = employeeRepository.select();
+
+        customRequest.put("table", maybeTable);
+
+        return "select";
+    }
+
+    public String update() throws Exception {
+
+        CustomRequest customRequest = CustomRequestContext.get();
+
+        EmployeeRepository employeeRepository = new EmployeeRepository();
+        Table maybeTable = employeeRepository.select();
+
+        for(int i = 0; i < maybeTable.size(); i++) {
+            String maybeEmpNo = maybeTable.find("EMP_NO", i);
+            customRequest.put("EMP_NO", maybeEmpNo);
+            if(employeeRepository.update() != 1) {
+                throw new Exception();
+            }
+        }
+        return select();
+    }
 }
--- inflearn/create-backend-framework/src/main/java/io/dailyworker/framework/domain/EmployeeRepository.java
+++ inflearn/create-backend-framework/src/main/java/io/dailyworker/framework/domain/EmployeeRepository.java
@@ -1,4 +1,22 @@
 package io.dailyworker.framework.domain;
 
+import io.dailyworker.framework.db.SqlRunner;
+import io.dailyworker.framework.db.Table;
+
+import java.sql.SQLException;
+
 public class EmployeeRepository {
+    private static final String SQL_SELECT = "SQL_SELECT_BY_USERNAME";
+    private static final String SQL_UPDATE = "SQL_UPDATE_EMP";
+
+    private final SqlRunner sql = SqlRunner.getSqlRunner();
+
+    Table select() throws SQLException {
+        return sql.getTable(SQL_SELECT);
+    }
+
+    int update() throws SQLException {
+        return sql.executeSql(SQL_UPDATE);
+    }
+
 }
