9e0589989d453f4a78e142927098a5d4e879da07
dailyworker <seansin@chungbuk.ac.kr>
2021-10-25 09:42:06 +0900

    feat : artillery-run.sh, config.yaml
    - 부하테스트용 쉘스크립트 및 부하테스트 설정 추가

---
+++ class101/FooBackend/artillery-script/artillery-run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# shellcheck disable=SC2039
+# shellcheck disable=SC2181
+REPORT_OUTPUT='report.json'
+RUN_YAML_FILE='config.yaml'
+
+which node &> /dev/null
+if [ $? == 0 ]; then
+ echo "Node Installed"
+ npm ls -g --depth=0 | grep -oP 'artillery' &> /dev/null
+  if [ $? == 0 ]; then
+    artillery run --output '{REPORT_OUTPUT}' '{RUN_YAML_FILE}'
+    artillery report '{REPORT_OUTPUT}'
+  else
+    npm install -g artillery
+    artillery run --output '{REPORT_OUTPUT}' '{RUN_YAML_FILE}'
+    artillery report '{REPORT_OUTPUT}'
+  fi
+ exit 0;
+else
+ echo "Node not installed"
+ exit 1;
+fi
+
