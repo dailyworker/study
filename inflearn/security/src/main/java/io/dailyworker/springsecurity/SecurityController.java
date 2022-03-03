569ac14b602a7f92c159b19be084c995c0945227
dailyworker <seansin@chungbuk.ac.kr>
2022-03-03 19:34:29 +0900

    feat : 스프링 시큐리티 강의 수강 중

--- inflearn/security/src/main/java/io/dailyworker/springsecurity/SecurityController.java
+++ inflearn/security/src/main/java/io/dailyworker/springsecurity/SecurityController.java
@@ -1,2 +1,17 @@
-package io.dailyworker.springsecurity;public class SecurityController {
+package io.dailyworker.springsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+    @GetMapping("/")
+    public String index() {
+        return "home";
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "loginPage";
+    }
 }
