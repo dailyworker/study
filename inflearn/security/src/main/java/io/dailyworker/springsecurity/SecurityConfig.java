0981e67cd582d25b095619a545c0a428482ea177
dailyworker <seansin@chungbuk.ac.kr>
2022-03-08 21:44:39 +0900

    study : 인가 API 설정

--- inflearn/security/src/main/java/io/dailyworker/springsecurity/SecurityConfig.java
+++ inflearn/security/src/main/java/io/dailyworker/springsecurity/SecurityConfig.java
@@ -2,6 +2,7 @@ package io.dailyworker.springsecurity;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -22,43 +23,33 @@ import java.io.IOException;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    UserDetailsService userDetailsService;
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}1111")
+                .roles("USER");
+
+        auth.inMemoryAuthentication()
+                .withUser("sys")
+                .password("{noop}1111")
+                .roles("SYS");
+
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}1111")
+                .roles("ADMIN");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests()
-            .anyRequest().authenticated()
-        .and()
-            .formLogin();
+                .authorizeRequests()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin/pay").hasRole("ADMIN")
+                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
+                .anyRequest().authenticated();
         http
-            .logout()
-            .logoutUrl("/logout")
-            .logoutSuccessUrl("/login")
-            .addLogoutHandler(new LogoutHandler() {
-                @Override
-                public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-                    HttpSession session = request.getSession();
-                    session.invalidate();
-                }
-            })
-            .logoutSuccessHandler(new LogoutSuccessHandler() {
-                @Override
-                public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                    response.sendRedirect("/login");
-                }
-            })
-            .deleteCookies("remember-me");
-
-        http
-            .rememberMe()
-            .rememberMeParameter("remember")
-            .tokenValiditySeconds(3600)
-            .userDetailsService(userDetailsService);
-        http
-            .sessionManagement()
-                .sessionFixation().changeSessionId();
-
+                .formLogin();
     }
 }
